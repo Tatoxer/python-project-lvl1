5b25dabc81fa4ace7f596389b40f61d9c04e8637
Anton Selifanov <Tatoxer@gmail.com>
2020-04-20 18:04:47 +0300

    mentor's comments 2

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,30 +6,21 @@ def run_game(game):
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name?\n")
     print('Hello, ' + name.capitalize() + "!\n")
-    repeat_game = True
+    print(game.DESCRIPTION)
+    count_of_right_answers = 0
+    score_to_win = 3
 
-    while repeat_game:
-        print(game.DESCRIPTION)
-        count_of_right_answers = 0
-        score_to_win = 3
+    while count_of_right_answers < score_to_win:
+        question, correct_answer = game.generate_game_data()
+        print("Question: " + question)
+        user_answer = prompt.string("Your answer: ")
 
-        while count_of_right_answers < score_to_win:
-            question, correct_answer = game.generate_game_data()
-            print("Question: " + question)
-            user_answer = prompt.string("Your answer: ")
+        if correct_answer != user_answer:
+            print(f'{user_answer} is wrong answer ;(')
+            print(f"Correct answer was {str(correct_answer)}. Let's try again")
+            break
+        print("Correct!")
+        count_of_right_answers += 1
 
-            if correct_answer != user_answer:
-                print(f'{user_answer} is wrong answer ;(')
-                print(f"Correct answer was {str(correct_answer)}. Let's try again")
-                break
-            print("Correct!")
-            count_of_right_answers += 1
-
-        if count_of_right_answers == score_to_win:
-            print(f'Congratulations, {name.capitalize()}!')
-
-        if prompt.string("\nDo you want to play again? yes/no\n") == 'yes':
-            repeat_game = True
-        else:
-            print(f'Good luck, {name.capitalize()}!')
-            repeat_game = False
+    if count_of_right_answers == score_to_win:
+        print(f'Congratulations, {name.capitalize()}!')
--- brain_games/game/progression.py
+++ brain_games/game/progression.py
@@ -6,18 +6,18 @@ DESCRIPTION = "What number is missing in the progression?"
 
 
 def generate_game_data():
-    progr = make_progression()
+    start = randint(1, 10)
+    difference = randint(1, 10)
+    progression_length = randint(8, 12)
+    progr = make_progression(start, difference, progression_length)
     correct_answer = choice(progr)
     question = game_question(correct_answer, progr)
     return str(question), str(correct_answer)
 
 
-def make_progression():
-    start = randint(1, 10)
-    difference = randint(1, 10)
+def make_progression(start, difference, progression_length):
     progression_ = []
     count = 0
-    progression_length = randint(8, 12)
 
     while count < progression_length:
         progression_.append(start + difference * count)
